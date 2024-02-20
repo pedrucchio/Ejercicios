4bb6ffaaf9157d75597164d7f1f6e9e3058c03fd
pedrucchio <pedrograndini@gmail.com>
2024-02-20 14:48:04 +0100

    Ejercicio 1 Javascript

---
+++ js/es1/conloo.js
@@ -0,0 +1,31 @@
+function calculateSalary(role) {
+    switch (role) {
+        case "ceo":
+            return "2200€"
+            break;
+        case "manager":
+            return "1800€"
+            break;
+        case "cto":
+            return "1800€"
+            break;
+        case "developer":
+            return "1500€";
+            break;
+        default:
+            return "1000€"
+            break;
+    }
+  }
+  
+  const ceoSalary = calculateSalary('ceo');
+  const managerSalary = calculateSalary('manager');
+  const ctoSalary = calculateSalary('cto');
+  const developerSalary = calculateSalary('developer');
+  const otherSalary = calculateSalary('other');
+  
+  console.log(ceoSalary);
+  console.log(managerSalary);
+  console.log(ctoSalary);
+  console.log(developerSalary);
+  console.log(otherSalary);
