d89c11caa8dc1c896722e7ddb4289477dd7e00b5
pedrucchio <pedrograndini@gmail.com>
2024-02-20 18:43:40 +0100

    Ejercicio 4 Javascript

---
+++ js/es4/conloop3.js
@@ -0,0 +1,9 @@
+function sumUntil(maxValue) {
+    let sum = 0;
+    for (let i = 1; i <= maxValue; i++) {
+        sum = sum + i;
+    }
+    return sum
+}
+    
+    console.log(sumUntil(5)); // OUTPUT: 15
