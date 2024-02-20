036311553e694ea6a9b3a66e2fe5b35af4f091da
pedrucchio <pedrograndini@gmail.com>
2024-02-20 18:50:36 +0100

    Objetos

---
+++ js/es5/objects.js
@@ -0,0 +1,16 @@
+const person1 = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 25,
+  };
+  
+  const person2 = {...person1};
+  person2.firstName = "Simon"
+  // Modify the property `firstName` of the `person2` in "Simon".
+
+  // la razon por la que al copiar normalmente cambia el original es porque lo que se 
+  // copia es la referencia al original por lo que al cambiar la copia como esta por \ referencia se 
+  // cambia tambien el original
+  
+  console.log(person1);
+  console.log(person2);
