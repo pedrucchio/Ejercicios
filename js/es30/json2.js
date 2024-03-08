748dac5f4f591b1efd8d0add5b327faf21a35fe1
pedrucchio <pedrograndini@gmail.com>
2024-03-08 16:16:03 +0100

    JSON 2

---
+++ js/es30/json2.js
@@ -0,0 +1,22 @@
+class Person {
+    constructor(id, firstName, lastName, age) {
+      this.id = id;
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.age = age;
+    }
+  
+    toJson() {
+      return JSON.stringify(this);
+    }
+
+    static fromJson(json){
+        const obj = JSON.parse(json);
+        const person = new Person(obj.id,obj.firstName,obj.lastName,obj.age)
+        return person;
+    }
+  }
+  
+  const json = '{"id":1,"firstName":"Mario","lastName":"Rossi","age":25}';
+  const developer = Person.fromJson(json);
+  console.log(developer);
