32ce60040e71e4c56b9bb2b5f6d52e3a7d6ae1d4
pedrucchio <pedrograndini@gmail.com>
2024-04-14 11:44:05 +0200

    Conditional

---
+++ react/conditional/src/Welcome.jsx
@@ -0,0 +1,18 @@
+import { Age } from "./Age";
+
+export function Welcome({ name = "World",age}) {
+    return (
+        <div>
+            <p>Welcome <strong>{ name }</strong></p>
+            <Age age = {17}/>
+            {18 < age && <Age age ={ 28 }/>}
+            {age && <Age age ={ 28 }/>}
+            {(18 < age && age < 65) && <Age age ={ 28 }/>}
+            {(18 < age && age < 65 && (name === "John")) && <Age age ={ 28 }/>}
+        </div>
+    )
+}
+
+
+
+            
