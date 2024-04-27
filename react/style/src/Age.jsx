c15d228734341f468c8bdd097a8d218cf09f5a61
pedrucchio <pedrograndini@gmail.com>
2024-04-27 16:26:01 +0200

    Styling Components

---
+++ react/style/src/Age.jsx
@@ -0,0 +1,3 @@
+export function Age({ age }) {
+    return 18 < age ? <p>Your age is { age }</p> : <p>You are very young!</p>
+}
---
+++ react/style/src/App.jsx
@@ -0,0 +1,12 @@
+import { Welcome } from './Welcome'
+
+function App() {
+
+  return (
+    <div>
+      <Welcome name='John' age={30}/>
+    </div>
+  )
+}
+
+export default App
