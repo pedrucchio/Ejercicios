db765efa2f1177b9501e9a9a184423ffcff60267
pedrucchio <pedrograndini@gmail.com>
2024-05-07 13:43:59 +0200

    React Router

--- react/route/src/App.jsx
+++ react/route/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { Welcome } from "./Welcome";
 import { Counter } from "./Counter"
 import GithubUser from "./GithubUser";
@@ -10,7 +10,13 @@ function App() {
         <Route path="/" element={<Welcome name="Juan" />}/>
         <Route path="/counter" element={ <Counter />} />
         <Route path="/users/:username" element={<GithubUser />} />
+        <Route path="*" element={<div>Not Found!</div>} />
       </Routes>
+      <div>
+				<Link to="/">Home</Link>
+				<Link to="/counter">Counter</Link>
+				<Link to="/users/pedrucchio">Pedro Grandini</Link>
+			</div>
     </BrowserRouter>
   )
 }
