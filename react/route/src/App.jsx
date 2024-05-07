fdeb6205343576f201fe16ceae614ed3eb46c33e
pedrucchio <pedrograndini@gmail.com>
2024-05-07 16:51:56 +0200

    React Router

--- react/route/src/App.jsx
+++ react/route/src/App.jsx
@@ -1,24 +1,33 @@
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-import { Welcome } from "./Welcome";
-import { Counter } from "./Counter"
-import GithubUser from "./GithubUser";
+import React from 'react';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { Welcome } from './Welcome';
+import { Counter } from './Counter';
+import GithubUser from './GithubUser';
+import { GithubUsers } from './GithubUsers';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Welcome name="Juan" />}/>
-        <Route path="/counter" element={ <Counter />} />
+        <Route path="/" element={<Welcome name="Juan" />} />
+        <Route path="/counter" element={<Counter />} />
+        <Route path="/users" element={<GithubUsers />} />
         <Route path="/users/:username" element={<GithubUser />} />
         <Route path="*" element={<div>Not Found!</div>} />
       </Routes>
       <div>
-				<Link to="/">Home</Link>
-				<Link to="/counter">Counter</Link>
-				<Link to="/users/pedrucchio">Pedro Grandini</Link>
-			</div>
+        <Link className="link-styles" to="/">
+          Home
+        </Link>
+        <Link className="link-styles" to="/counter">
+          Counter
+        </Link>
+        <Link className="link-styles" to="/users">
+          Users
+        </Link>
+      </div>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ react/route/src/GithubUsers.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+export function GithubUsers() {
+	const [users, setUsers] = useState([]);
+	const [error, setError] = useState(null);
+	const [loading, setLoading] = useState(false);
+
+	useEffect(() => {
+		setLoading(true);
+		fetch(`https://api.github.com/users`)
+			.then((res) => res.json())
+			.then((json) => setUsers(json))
+			.catch((error) => setError(error))
+			.finally(() => setLoading(false));
+	}, []);
+
+	return (
+		<div>
+			{users.length && (
+				<ul data-testid='users'>
+					{users.map((user) => (
+						<li key={user.id}>
+							<Link to={`/users/${user.login}`}>{user.login}</Link>
+						</li>
+					))}
+				</ul>
+			)}
+			{error && <div>{error.message}</div>}
+			{loading && <div>Loading...</div>}
+		</div>
+	);
+}
