2fc929fe7a2a5a85708e39757ceefc74a2cbb368
pedrucchio <pedrograndini@gmail.com>
2024-05-17 09:07:50 +0200

    Primitives

---
+++ typescript/es1/exercise-1-primitive-types-start.js
@@ -0,0 +1,18 @@
+"use strict";
+/**
+ * START: Follow the instructions below.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+// Add type annotations to each of these variables.
+var country = "Italy";
+var year = undefined;
+var averageTemperature = 23.6;
+var visited = true;
+var currency = null;
+// Change the type annotations on these variables so they are correct.
+// let population: boolean = 47_450_795;
+var population = 47450795;
+// let isSummer: string = false;
+var isSummer = false;
+// let languages: number = "Spanish, English";
+var languages = "Spanish, English";
---
+++ typescript/es1/exercise-1-primitive-types-start.ts
@@ -0,0 +1,30 @@
+/**
+ * START: Follow the instructions below.
+ */
+
+// Add type annotations to each of these variables.
+
+let country: string = "Italy";
+
+let year: any = undefined;
+
+let averageTemperature: number = 23.6;
+
+let visited:boolean = true;
+
+let currency: any = null;
+
+// Change the type annotations on these variables so they are correct.
+
+// let population: boolean = 47_450_795;
+let population: number = 47_450_795;
+
+// let isSummer: string = false;
+let isSummer: boolean = false;
+
+// let languages: number = "Spanish, English";
+let languages: string = "Spanish, English";
+
+// ----
+
+export {};
