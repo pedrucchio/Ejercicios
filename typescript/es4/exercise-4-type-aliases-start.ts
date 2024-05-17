9eee616c6043f877f2a523c9a617d38f9378184e
pedrucchio <pedrograndini@gmail.com>
2024-05-17 16:27:20 +0200

    Type aliases

---
+++ typescript/es4/exercise-4-type-aliases-start.ts
@@ -0,0 +1,21 @@
+/**
+ * START: Follow the instructions below.
+ */
+
+// Create a `Country` type alias for the `string` type.
+// Change the type for each variable to use the `Country` type.
+
+type Country = string
+
+// const country1: string = "Nigeria";
+const country1:Country = "Nigeria";
+
+// const country2: string = "Italy";
+const country2:Country = "Italy"
+
+// const country3: string = "China";
+const country3:Country = "China";
+
+// ----
+
+export {};
