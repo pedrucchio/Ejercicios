f4ba12d01b575ef56665de608acffdf5a033a54a
pedrucchio <pedrograndini@gmail.com>
2024-05-23 17:35:24 +0200

    Create Table

---
+++ sql/createtable.sql
@@ -0,0 +1,20 @@
+-- TABLE
+CREATE TABLE Books (
+	book_id INTEGER PRIMARY KEY,
+  	title TEXT NOT NULL,
+  	author TEXT NOT NULL,
+  	genre TEXT NOT NULL,
+  	published_year INTEGER NOT NULL,
+   	isbn TEXT NOT NULL,
+  	price FLOAT NOT NULL,
+  	rating INTEGER NOT NULL,
+  	stock_count INTEGER NOT NULL
+);
+
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
