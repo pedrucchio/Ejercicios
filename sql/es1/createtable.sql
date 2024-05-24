387c92eeaddac0e4b057bfea587499b64d180069
pedrucchio <pedrograndini@gmail.com>
2024-05-24 13:00:59 +0200

    CREATE ALTER INSERT

--- sql/es1/createtable.sql
+++ sql/es1/createtable.sql
@@ -1,14 +1,14 @@
 -- TABLE
 CREATE TABLE Books (
-	book_id INTEGER PRIMARY KEY,
-  	title TEXT NOT NULL,
-  	author TEXT NOT NULL,
-  	genre TEXT NOT NULL,
-  	published_year INTEGER NOT NULL,
-   	isbn TEXT NOT NULL,
-  	price FLOAT NOT NULL,
-  	rating INTEGER NOT NULL,
-  	stock_count INTEGER NOT NULL
+	book_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  	title varchar(20),
+  	author varchar(20),
+  	genre TEXT varchar(20),
+  	published_year INTEGER,
+   	isbn varchar(20),
+  	price DECIMAL(10,2),
+  	rating DECIMAL(2,2),
+  	stock_count INTEGER
 );
 
  
--- sql/es2/Altertable.sql
+++ sql/es2/Altertable.sql
@@ -1,5 +1,5 @@
 ALTER TABLE Books
-ADD COLUMN publisher TEXT;
+ADD COLUMN publisher varchar(20);
 
 ALTER TABLE Books
 ADD COLUMN number_pages INTEGER;
