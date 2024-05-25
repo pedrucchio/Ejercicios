564a4fcb9db73794c7cb76e22d04d72dba940fd6
pedrucchio <pedrograndini@gmail.com>
2024-05-25 10:38:49 +0200

    Grant

--- sql/es5/Grant.sql
+++ sql/es5/Grant.sql
@@ -2,7 +2,7 @@
 CREATE USER IF NOT EXISTS 'martin'@'localhost' IDENTIFIED BY 'your_password';
 
 -- Step 2: Grant SELECT and UPDATE permissions on the Books table
-GRANT SELECT, UPDATE ON bookstore.Books TO 'martin'@'localhost';
+GRANT SELECT, UPDATE ON Books TO 'martin'@'localhost';
 
 -- Step 3: Revoke DELETE permissions on the Books table just in case they were previously granted
-REVOKE DELETE ON bookstore.Books FROM 'martin'@'localhost';
+REVOKE DELETE ON Books FROM 'martin'@'localhost';
